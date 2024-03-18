272471e88d18646d80dc4ae7b6cea1b1d46668d8
Suraj Sharma <suraj200delhi@gmail.com>
2024-03-18 13:45:56 +0530

    lecture 02 and readme update

--- Lecture_02/src/components/Navbar.jsx
+++ Lecture_02/src/components/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 
-const Navbar = () => {
+const Navbar = (prop) => {
  
-   const [count,setcount] = useState(0)
+   const [count,setcount] = useState(0);
+
    useEffect(()=>{
-    setcount(count + 1)
+    console.log("count changed");
    },[count])
+
+   useEffect(()=>{
+console.log("run on every render");
+   })
+
+   // Example of cleanup function
+useEffect(()=>{
+  console.log("Hey welcome to my page");
+  return()=>{
+    alert("Navbar was unmounted")
+  }
+  },[])
 
   return (
    <>
-   <button></button>
+   <h2>Effect {prop.color}{count}</h2>
+<button onClick={()=>{setcount(count+1)}}>Use Effect btn </button>
    </>
   )
 }
