e89184bfbf05ef14abcebfd509839998835efc10
Suraj Sharma <suraj200delhi@gmail.com>
2024-04-17 21:32:22 +0530

    Add local storage to todo

--- Lecture_06/src/App.jsx
+++ Lecture_06/src/App.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { useState } from 'react';
 import { uid } from 'uid';
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { MdAddTask } from "react-icons/md";
-const App = () => {
 
+
+const App = () => {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    let data = JSON.parse(localStorage.getItem("todos"))
+    if (data !== null && data.length > 0) {
+      setTodos(data);
+      console.log(data);
+    } else {
+      console.log("No todos");
+    }
+  }, [])
+
+  const saveTodo = () => {
+    try {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    } catch (error) {
+      console.error("Error is :", error);
+    }
+  }
+
+  useEffect(() => {
+    saveTodo()
+  }, [todos])
+
 
   const onChangeHandler = (e) => {
     setTodo(e.target.value)
   };
+
+  const addTodo = () => {
+    todos === "" ? alert("Enter Something") :
+      setTodos([...todos, { id: uid(), todo, iscomplete: false }])
+    setTodo("")
+  };
+
+  const keyDownHandler = (e) => {
+    if (e.key === "Enter") {
+      addTodo()
+    }
+  }
 
   const checkHandler = (e) => {
     let id = e.target.name;
@@ -26,22 +62,21 @@ const App = () => {
   };
 
   const editHandler = (id) => {
-    let index = todos.findIndex((item)=>{
-      return  item.id ===id
+    let index = todos.findIndex((item) => {
+      return item.id === id
     })
     setTodo(todos[index].todo)
 
-    const newTodos = todos.filter((item)=>{
+    const newTodos = todos.filter((item) => {
       return item.id !== id
     })
-   
+
     console.log(newTodos);
     setTodos(newTodos)
-
   };
 
   const deleteHandler = (id) => {
-   
+
     const index = todos.findIndex((item) => {
       return item.id === id
     })
@@ -52,42 +87,41 @@ const App = () => {
     console.log(newTodos)
     setTodos(newTodos)
   };
-
-
-  const addTodo = () => {
-    if (todo === "") {
-      alert("Write Something")
+  const todoStatusHandler =(e)=>{
+    let todoStatus = e.target.value
+    if (todoStatus==="all") {
+     
     }
-    else {
-
-      setTodos([...todos, { id: uid(), todo, iscomplete: false }])
-      setTodo("")
-    }
-    
-  };
-
+  }
 
 
 
   return (
     <>
       <Navbar />
-      <div className='min-h-screen bg-black '>
+      <div className='min-h-screen bg-black font-Roboto '>
         <div className="container flex p-10 ">
-          <input onChange={onChangeHandler} value={todo} type="text" className=' w-1/2 mx-2 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:border-blue-500'  />
+          <input onKeyDown={keyDownHandler} onChange={onChangeHandler} value={todo} type="text" className=' w-1/2 mx-2 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:border-blue-500' />
           <button onClick={addTodo} type="button" className='mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'><MdAddTask /></button>
         </div>
         {todos.length === 0 && <h1 className='  ml-8 bold text-white'>No Todos to Display</h1>}
-        {todos.map((item) => {
-          return <div key={uid()} className="todocontainer mt-4 ml-8 text-white flex ">
-            <div className="todoScontainer w-1/2 flex">
-              <input onChange={checkHandler} checked={item.iscomplete} type="checkbox" name={item.id} id="" />
-              <div className={` bg-white text-black mx-2 rounded-lg p-2 ${item.iscomplete ? "line-through" : ""}`}>{item.todo}</div>
-            </div>
-            <div className="btns">
+        {todos.length !== 0 &&
+          <select onChange={todoStatusHandler} className='ml-10 p-2 rounded bg-blue-950 text-white' name="todos" >
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="not_completed">Not Completed</option>
+          </select>}
 
-              <button onClick={()=>{editHandler(item.id)}} name={item.id} className='mx-2 bg-green-500 hover:bg-green-700 text-white font-bold  p-5 rounded'><FaEdit /></button>
-              <button onClick={ () => {deleteHandler(item.id)} }name={item.id} className='mx-2 bg-red-500 hover:bg-red-700 text-white font-bold  p-5 rounded '><MdDelete /></button>
+        {todos.map((item) => {
+          return <div key={uid()} className="todocontainer mt-4 ml-8 w-1/2 text-white flex justify-between ">
+            <div className="todoScontainer gap-4 flex ">
+              <input onChange={checkHandler} checked={item.iscomplete} type="checkbox" name={item.id} id="" />
+              <div className={` h-full bg-white  text-black mx-2 rounded-lg p-5 ${item.iscomplete ? "line-through" : ""}`}>{item.todo}</div>
+            </div>
+            <div className="btns flex  h-full" >
+
+              <button onClick={() => { editHandler(item.id) }} name={item.id} className='mx-2 bg-green-500 hover:bg-green-700 text-white font-bold  p-5 rounded'><FaEdit /></button>
+              <button onClick={() => { deleteHandler(item.id) }} name={item.id} className='mx-2 bg-red-500 hover:bg-red-700 text-white font-bold  p-5 rounded '><MdDelete /></button>
             </div>
           </div>
 
