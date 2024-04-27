f9190909851d6718b8ce89b7d387a31b4d25c6c2
Suraj Sharma <suraj200delhi@gmail.com>
2024-04-27 09:49:22 +0530

    Pass_generator

--- Lecture_07/src/App.jsx
+++ Lecture_07/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState ,useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
   const [passLength, setPassLength] = useState(8)
   const [number, setNumber] = useState(false)
   const [special, setSpecial] = useState(false)
+  const passRef =useRef()
 
   const passgenerator = () => {
     let alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -17,12 +18,9 @@ function App() {
     if (special) {
       str += "./?!@#$%^&*|\/><"
     }
-    console.log(str);
     for (let i = 0; i < passLength; i++) {
       let index = Math.floor(Math.random() * str.length + 1);
-      console.log(index);
       pass = pass + str.charAt(index)
-      console.log(pass);
       setPass(pass)
     }
   }
@@ -33,40 +31,45 @@ function App() {
 
   useEffect(() => {
     passgenerator()
-  }, [number,special,passLength])
+  }, [number, special, passLength])
 
- 
-
-  const numClickHandler=()=>{
-    setNumber(!number)
-    console.log(number);
+  const copyPassword =()=>{
+    window.navigator.clipboard.writeText(pass)
+    passRef.current?.select()
+    
   }
 
-  const specClickHandler=()=>{
+  const numClickHandler = () => {
+    setNumber(!number)
+  }
+
+  const specClickHandler = () => {
     setSpecial(!special)
-    console.log(special);
   }
   return (
-    < >
-      <div className=" min-h-screen text-white  bg-black">
-
-        <div className="container  ">
-          <div className="scontainer border w-1/2 rounded   p-3'">
-            <h1 className=' '> password Generator</h1>
-            <input className='text-black' type="text " name="" id="" readOnly value={pass} />
+    <>
+      <div className="min-h-screen text-black bg-black flex justify-center items-center">
+        <div className="container border-transparent bg-gray-800 p-4 rounded-lg w-1/2">
+          <h1 className=" text-2xl   text-white mb-4">Basic Password Generator</h1>
+          <div className='flex'>
+          <input ref={passRef} className="mt-2 mb-4 p-2 w-full rounded border" type="text" readOnly value={pass} />
+          <button  onClick={copyPassword} className='bg-blue-500 hover:bg-blue-700 text-white font-bold  h-11 mt-2 mx-2 px-4  rounded'>Copy</button>
           </div>
-
-          <div className='scontainer2'>
-            <input onChange={(e) => { setPassLength(e.target.value) }} value={passLength} min={8} max={20} step={1} defaultValue={10} type="range" /><label htmlFor="lenght">password Lenght  is {passLength}</label>
-            <label  htmlFor="number">Number</label><input onClick={numClickHandler} type="checkbox" name="number" id="" />
-            <label  htmlFor="special">SpecialKeyword</label><input onClick={specClickHandler} type="checkbox" name="specialkeyword" id="" />
+          <div className="flex items-center mb-4  text-orange-400">
+            <input className='w-1/3' onChange={(e) => { setPassLength(e.target.value); }} value={passLength} min={8} max={20} step={1} defaultValue={10} type="range" />
+            <label className="ml-2" htmlFor="length">Password Length is {passLength}</label>
+          </div>
+          <div className="flex items-center text-orange-400">
+            <input onChange={numClickHandler} type="checkbox" className="mr-2" name="number" id="number" />
+            <label htmlFor="number">Include Numbers</label>
+          </div>
+          <div className="flex items-center text-orange-400 mt-2">
+            <input onChange={specClickHandler} type="checkbox" className="mr-2" name="special" id="special" />
+            <label htmlFor="special">Include Special Characters</label>
           </div>
         </div>
-
       </div>
-    </>
-  )
+    </>)
 }
-
 
 export default App
